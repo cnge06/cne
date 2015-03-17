61863f7c9ebe7ca9ba6b49fecbf30ba949f8c906
cnge06 <cnge06@163.com>
2015-03-17 16:47:39 +0800

    update

---
+++ src/main/java/com/cnge06/cne/protobuf/Response.java
@@ -0,0 +1,47 @@
+package com.cnge06.cne.protobuf;
+
+import io.netty.channel.Channel;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+import com.google.protobuf.GeneratedMessage;
+import com.msg.Msg.Protocol;
+
+public class Response {
+	private static ConcurrentMap<Channel,Response> responseMap=new ConcurrentHashMap<Channel,Response>();
+
+	private Response(){}
+
+	private Channel channel;
+	
+	public static Response getResponse(Channel channel){
+		Response response = responseMap.get(channel);
+		if(response==null){
+			response=new Response();
+			response.setChannel(channel);
+		}
+		return response;
+	}
+	
+	public void write(GeneratedMessage gm){
+		Protocol.Builder pb=Protocol.newBuilder();
+		pb.setData(gm.toByteString());
+		pb.setPath("user");
+		//Channel channel=getChannel();
+		channel.write(pb);
+		channel.flush();
+	}
+
+	public Channel getChannel() {
+		return channel;
+	}
+
+	public void setChannel(Channel channel) {
+		this.channel = channel;
+	}
+	
+	
+	
+	
+}
